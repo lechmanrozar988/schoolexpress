4f77f57c89c88c9864fb4caa4123db4ead41b8b5
lechmanrozar988 <lechmanrozar988@gmail.com>
2025-05-23 06:31:23 +0000

    Add 41.php

---
+++ src/41.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * School Projects Management System
+ *
+ * Copyright (C) 2023 - present by School Project Team <schoolprojectteam@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Project information
+
+// Project name
+project_name = "My School Projects";
+
+// Project description
+project_description = "This is my school project management system.";
+
+// Team members
+team_members = [
+    "John Doe",
+    "Jane Smith",
+];
+
+// Team email
+team_email = "schoolprojectteam@gmail.com";
+
+// System version
+system_version = "1.0.0";
+?>
