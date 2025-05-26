e41f268034df5815dec988f0c656f34032f8dff5
lechmanrozar988 <lechmanrozar988@gmail.com>
2025-05-26 01:36:04 +0000

    Add 43.php

---
+++ src/43.php
@@ -0,0 +1,13 @@
+<?php
+// Initialize database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "schoolexpress";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
